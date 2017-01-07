87cc93ac3cf449cc41e4b2af0be98c2236018afa
Michel Langeveld <michel.langeveld@xs4all.nl>
2017-01-07 12:32:53 +0100

    commandline arguments

--- src/Improve.java
+++ src/Improve.java
@@ -7,7 +7,7 @@ public class Improve implements SolutionPrinter {
     private double maxArea = 0.0;
     private Point[] maxPoints = null;
     private Point[] minPoints = null;
-    private boolean higher = false;
+    private static boolean higher = false;
 
     public void solution(Point[] solution) {
         int area = generator.getArea();
@@ -30,12 +30,15 @@ public class Improve implements SolutionPrinter {
 
     public static void main(String args[]) throws Exception {
         Improve improve = new Improve();
-        improve.doit();
+        higher = args[0].equals("max");
+        String polygon = "(36,40), (43,45), (35,43), (25,41), (41,44), (32,37), (30,36), (22,27), (29,35), (10,17), (26,47), (4,6), (1,2), (5,5), (2,3), (7,7), (14,1), (6,8), (8,11), (3,4), (9,15), (13,14), (17,10), (12,13), (18,9), (16,12), (11,16), (15,21), (19,25), (24,28), (20,19), (31,29), (21,20), (27,32), (23,22), (28,33), (34,38), (37,39), (38,31), (42,26), (33,34), (44,24), (46,18), (45,23), (39,30), (40,42), (47,46)";
+        polygon = args[1];
+        improve.doit(polygon);
     }
 
-    private void doit() {
+    private void doit(String s) {
         long start = System.nanoTime();
-        improve("(22,25), (24,28), (55,32), (40,31), (49,38), (46,33), (53,41), (58,27), (51,47), (45,45), (50,46), (52,40), (44,35), (41,42), (37,48), (42,39), (43,34), (35,36), (33,43), (34,37), (29,44), (30,49), (56,55), (47,53), (61,58), (68,64), (64,61), (65,65), (70,68), (66,54), (71,71), (59,66), (67,69), (48,60), (63,67), (54,62), (69,70), (60,59), (62,63), (57,57), (38,51), (27,50), (10,56), (21,52), (39,30), (17,29), (9,8), (4,4), (3,3), (6,7), (2,2), (5,11), (1,1), (7,6), (8,5), (11,12), (15,13), (12,15), (14,16), (19,10), (16,14), (20,9), (13,18), (18,26), (23,24), (26,19), (25,23), (32,20), (36,17), (31,21), (28,22)");
+        improve(s);
         long duration = System.nanoTime() - start;
         System.out.println("duration: " + (double) duration / 1000000);
         System.out.println("solutions: " + generator.numberOfSolutions);
@@ -48,7 +51,7 @@ public class Improve implements SolutionPrinter {
 
         start:
 
-        for (int maxaantal=0; maxaantal< 9; maxaantal++) {
+        for (int maxaantal=0; maxaantal< 9999; maxaantal++) {
 
             System.out.println("maxaantal: " + maxaantal);
 
